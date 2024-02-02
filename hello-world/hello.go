03d4a841df2be4af8a0faf746be02dc24b2ef2c5
Monark Basak <monarkb01@gmail.com>
2024-02-01 22:33:03 -0500

    Completed the hello world chapter.

---
+++ hello-world/hello.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+// prefix is a named return
+// meaning that this function has a variable called prefix initialized to "" since it's a string
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
